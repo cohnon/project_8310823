2fa29dcdda79ba5ef897a12f443df835a4e9a8e7
unknown <ceasedinsanity.ct@gmail.com>
2021-04-15 08:05:05 -0300

    Fixed Github Pages Link

--- install.js
+++ install.js
@@ -3,5 +3,6 @@ window.sprite_renderer = document.createElement("script");
 if (!window.sprite_renderer)
 {
   window.sprite_renderer.setAttribute("type", "text/javascript");
-  window.sprite_renderer.setAttribute("src", "https://coherentnonsense.github.io/sprint-client/project_8310823/out.js");
+  window.sprite_renderer.setAttribute("src", "https://coherentnonsense.github.io/project_8310823/out.js");
+  document.body.appendChild(window.sprite_renderer);
 }
