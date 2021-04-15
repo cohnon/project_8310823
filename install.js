c6c6eb983428bb6e0508f1e70b4d261bfa78d4f9
unknown <ceasedinsanity.ct@gmail.com>
2021-04-15 08:08:22 -0300

    Dumn singleton error

--- install.js
+++ install.js
@@ -1,7 +1,6 @@
-window.sprite_renderer = document.createElement("script");
-
 if (!window.sprite_renderer)
 {
+  window.sprite_renderer = document.createElement("script");
   window.sprite_renderer.setAttribute("type", "text/javascript");
   window.sprite_renderer.setAttribute("src", "https://coherentnonsense.github.io/project_8310823/out.js");
   document.body.appendChild(window.sprite_renderer);
